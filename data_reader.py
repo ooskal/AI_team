f9ae3c174c95791ec45c886609aa56087a2fa050
tud0 <psy563012@gmail.com>
2022-06-06 10:58:58 +0900

    데이터리더파일변경
    데이터 정규화 기능 추가 , 파일 이름 변경

--- data_reader.py
+++ data_reader.py
@@ -8,6 +8,7 @@ Created on Fri Jun  3 09:19:12 2022
 
 import numpy as np
 import random
+import pandas as pd
 
 
 class DataReader():
@@ -16,9 +17,9 @@ class DataReader():
         #훈련, 테스트 데이터
         self.x_train, self.y_train, self.x_test, self.y_test = self.read_data()
         #데이터 정보 출력 
-        print("******** Data Read Done ********")
+        print("\n******** Data Read Done ********")
         print("Training Data Size: " + str(len(self.x_train)))
-        print("Test Data Size: " + str(len(self.x_test)) + "\n\n")
+        print("Test Data Size: " + str(len(self.x_test)) + "\n")
         
         
     # 데이터 읽어오기 위한 메서드
@@ -27,15 +28,15 @@ class DataReader():
         file = open("emails.csv")
         file.readline()
         
+        max_num= self.get_max()
+        
         # 데이터, 레이블 저장할 변수
         data = []
         
         # 파일 한 줄씩 읽어오기
         for line in file:
-            splt = line.split(",") # 컴마 기준으로 split()실행, 문자열 > 리스트
-            
-            x, y = self.process_data(splt) # split 결과물 정리 후 x, y 값으로 나누기
-            
+            splt = line.split(",") # 컴마 기준으로 split()실행
+            x, y = self.process_data(splt, max_num) # split 결과물 정리
             data.append((x, y)) # 나누어진 데이터 저장
             
         # 데이터 섞기
@@ -61,25 +62,31 @@ class DataReader():
     
     
     # split() 값 정리하는 메서드
-    def process_data(self, splt):
+    def process_data(self, splt, max_num):
         
         # 특징들 저장할 변수
         features = []
         
+        # 데이터 정규화
         for i in range(1, 3001):
-            features.append(float(splt[i]))
+            features.append(float(splt[i]) / float(max_num[i-1]))
         
         # 레이블 저장할 변수
         label = float(splt[3001])
         
-        
         return features, label
+    
+    # 특징별 최댓값 구하는 메서드
+    def get_max(self):
         
+        max_val = []
+        csv = pd.read_csv('emails.csv')
         
+        for i in range(1, 3001):
+            col = csv.iloc[:, i].values.tolist()
+            max_val.append(np.max(col))
         
-        
-        
-        
+        return max_val
         
         
         
